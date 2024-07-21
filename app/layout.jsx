e56c73a004b82866801d84dc123ba41cbc134f3e
J0hn316 <jcordell316@gmail.com>
2024-07-21 00:34:57 -0400

    first commit

---
+++ app/layout.jsx
@@ -0,0 +1,35 @@
+// styles
+import '@/app/_styles/globals.css';
+import { Josefin_Sans } from 'next/font/google';
+
+const fontStyle = Josefin_Sans({
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+// components
+import Header from './_components/Header';
+
+export const metadata = {
+  title: {
+    template: '%s / The Wild Oasis',
+    default: 'Welcome / The Wild Oasis',
+  },
+  description:
+    'Luxurious cabin hotel, located in the heart of the Italian Dolomites, surrounded by beautiful mountains and dark forests.',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${fontStyle.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col antialiased relative`}
+      >
+        <Header />
+        <div className="flex-1 px-8 py-12 grid">
+          <main className="max-w-7xl mx-auto w-full">{children}</main>
+        </div>
+      </body>
+    </html>
+  );
+}
