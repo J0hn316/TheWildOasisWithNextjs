120978ec4ab499ffb54838ec5e92792a85702152
J0hn316 <jcordell316@gmail.com>
2024-07-23 18:35:35 -0400

    next commit

--- app/_components/UpdateProfileForm.jsx
+++ app/_components/UpdateProfileForm.jsx
@@ -1,20 +1,40 @@
 'use client';
 
 import { useState } from 'react';
+import { useFormStatus } from 'react-dom';
+import { updateProfile } from '../_lib/actions';
 
-function UpdateProfileForm({ children }) {
+function Button() {
+  const { pending } = useFormStatus();
+
+  return (
+    <button
+      className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
+      disabled={pending}
+    >
+      {pending ? 'Updating profile' : 'Update Profile'}
+    </button>
+  );
+}
+
+function UpdateProfileForm({ guest, children }) {
   const [count, setCount] = useState();
-  // CHANGE
-  const countryFlag = 'pt.jpg';
-  const nationality = 'portugal';
+  const { fullName, email, nationality, nationalID, countryFlag } = guest;
+
+  useFormStatus();
 
   return (
     <div>
-      <form className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col">
+      <form
+        action={updateProfile}
+        className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col"
+      >
         <div className="space-y-2">
           <label>Full name</label>
           <input
             disabled
+            defaultValue={fullName}
+            name="fullName"
             className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
           />
         </div>
@@ -23,6 +43,8 @@ function UpdateProfileForm({ children }) {
           <label>Email address</label>
           <input
             disabled
+            defaultValue={email}
+            name="email"
             className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
           />
         </div>
@@ -42,14 +64,13 @@ function UpdateProfileForm({ children }) {
           <label htmlFor="nationalID">National ID number</label>
           <input
             name="nationalID"
+            defaultValue={nationalID}
             className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
           />
         </div>
 
         <div className="flex justify-end items-center gap-6">
-          <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
-            Update profile
-          </button>
+          <Button />
         </div>
       </form>
     </div>
--- app/account/page.jsx
+++ app/account/page.jsx
@@ -1,9 +1,18 @@
+import { auth } from '../_lib/auth';
+
 export const metadata = {
   title: 'Guest',
 };
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth();
+
+  // session.user.name returns the full name of user. To just display the first name do the following.
+  const firstName = session.user.name.split(' ').at(0);
+
   return (
-    <h2 className="font-semibold text-2xl text-accent-400 mb-7">Welcome</h2>
+    <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+      Welcome {firstName}
+    </h2>
   );
 }
