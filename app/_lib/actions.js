b226201b9626df3deca40773917ef8739bdab978
J0hn316 <jcordell316@gmail.com>
2024-07-25 14:37:23 -0400

    next push

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -13,6 +13,30 @@ export async function signOutAction() {
   await signOut({ redirectTo: '/' });
 }
 
+export async function createBooking(bookingData, formData) {
+  const session = await auth();
+  if (!session) throw new Error('You must be logged in.');
+
+  const newBooking = {
+    ...bookingData,
+    guestId: session.user.guestId,
+    numGuests: Number(formData.get('numGuest')),
+    observations: formData.get('observations').slice(0, 1000),
+    extraPrice: 0,
+    totalPrice: bookingData.cabinPrice,
+    isPaid: false,
+    hasBreakfast: false,
+    status: 'unconfirmed',
+  };
+
+  const { error } = await supabase.from('bookings').insert([newBooking]);
+
+  if (error) throw new Error('Booking could not be created');
+
+  revalidatePath(`/cabins/${bookingData.cabinId}`);
+  redirect('/cabins/thankyou');
+}
+
 export async function updateProfile(formData) {
   // 1) Authentication
   const session = await auth();
